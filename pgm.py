4955fe1233e6bed2251f23fb684941a72206c225
Stepan <kryzhankov.stepan@mail.ru>
2022-10-12 13:30:21 +0300

    feat: paint and save p5 p6 images

--- pgm.py
+++ pgm.py
@@ -1,4 +1,9 @@
+import array
+
 import numpy as np
+
+image_paint_path = "./images/paint.pgm"
+image_sample_path = "./images/sample_640×426.pgm"
 
 
 # P5
@@ -9,8 +14,25 @@ def read_pgm(file):
     return [[ord(file.read(1)) for _ in range(width)] for _ in range(height)]
 
 
-def crete_raster_sample_map():
-    pgm_path = "./sample_640×426.pgm"
+def create_pgm(pixel_map):
+    header = b'P5\n'
+    size = bytes(f"{len(pixel_map[0])} {len(pixel_map)}\n", encoding='utf-8')
+    depth = b'255\n'
+    f = open(image_paint_path, 'wb')
+    f.write(header)
+    f.write(size)
+    f.write(depth)
+    buff = array.array('B')
+    for row in pixel_map:
+        for c in row:
+            buff.append(int(c))
+    buff.tofile(f)
+    f.close()
+
+
+def crete_raster_sample_map(pgm_path):
+    if pgm_path is None:
+        pgm_path = image_sample_path
     f = open(pgm_path, 'rb')
     im = read_pgm(f)
     f.close()
--- ppm.py
+++ ppm.py
@@ -1,4 +1,9 @@
+import array
+
 import numpy as np
+
+image_paint_path = "./images/paint.ppm"
+image_sample_path = "./images/sample_640×426.ppm"
 
 
 # P6
@@ -9,8 +14,26 @@ def read_ppm(file):
     return [[[ord(file.read(1)) for _ in range(3)] for _ in range(width)] for _ in range(height)]
 
 
-def crete_raster_sample_map():
-    pgm_path = "./sample_640×426.ppm"
+def create_ppm(pixel_map):
+    header = b'P6\n'
+    size = bytes(f"{len(pixel_map[0])} {len(pixel_map)}\n", encoding='utf-8')
+    depth = b'255\n'
+    f = open(image_paint_path, 'wb')
+    f.write(header)
+    f.write(size)
+    f.write(depth)
+    buff = array.array('B')
+    for row in pixel_map:
+        for c in row:
+            for k in c:
+                buff.append(int(k))
+    buff.tofile(f)
+    f.close()
+
+
+def crete_raster_sample_map(pgm_path):
+    if pgm_path is None:
+        pgm_path = image_sample_path
     f = open(pgm_path, 'rb')
     im = read_ppm(f)
     f.close()
